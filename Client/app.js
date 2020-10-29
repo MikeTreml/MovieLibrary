207ba433377cde07f203673983c3db049a9272d2
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-29 14:29:32 -0500

    Add search button + logic for Get

--- Client/app.js
+++ Client/app.js
@@ -23,5 +23,30 @@
         e.preventDefault();
     }
 
-    $('#my-form').submit( processForm );
+    function processFormXX( e ){
+        var dict = {
+            Title : this["title"].value,
+            Genre: this["genre"].value,
+        	Director: this["director"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#create-or-search-form').submit( processForm );
+    $('#create-or-search-form').search( processFormXX )
 })(jQuery);
