6ccd31d61b8ea10cdf8f17802b32f7f37274d2a6
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-03 05:29:06 -0600

    Replace dead img hyperlinks in DB

---
+++ WebAPISample/Migrations/20201103112635_initial2.cs
@@ -0,0 +1,279 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPISample.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 11,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_l9zxfzAxwt1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 13,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_md0l1j2hdo1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 14,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_meh4v9uTiv1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 15,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_lkvjrd21N81qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 16,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_mcq2kfOLls1qe2w1uo2_250.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 17,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/6c2c193a6fe15405cdfee0bca1015f88/tumblr_mew3vxzum41qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 18,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_lptntapHmc1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 19,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_m8ley03LkX1qe2w1uo3_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 20,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_lvh12nTpdB1qe2w1uo1_400.png");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 21,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_m510rgIxfe1qe2w1uo1_500.png");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 22,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_lim74k13Vo1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 23,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_lg1elfr9v51qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 24,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_lisk13i2Xt1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 26,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_llki7u7rvy1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 27,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_m9yjhg468k1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 28,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_m6wmymcMUp1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 29,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_mb9lrfiJWU1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 30,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_m12uw1dIcX1qe2w1uo1_500.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 31,
+                column: "ImageURL",
+                value: "https://64.media.tumblr.com/tumblr_leq2dcTbs81qe2w1uo1_500.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 11,
+                column: "ImageURL",
+                value: "https://i.imgur.com/UQ8Fz.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 13,
+                column: "ImageURL",
+                value: "https://i.imgur.com/k3FvWcS.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 14,
+                column: "ImageURL",
+                value: "https://i.imgur.com/rfxuSWh.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 15,
+                column: "ImageURL",
+                value: "https://i.imgur.com/J7HKByA.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 16,
+                column: "ImageURL",
+                value: "https://i.imgur.com/i9NSYmz.png");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 17,
+                column: "ImageURL",
+                value: "https://i.imgur.com/1h11H0u.png");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 18,
+                column: "ImageURL",
+                value: "https://i.imgur.com/s1GHCJg.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 19,
+                column: "ImageURL",
+                value: "https://i.imgur.com/two02c7.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 20,
+                column: "ImageURL",
+                value: "https://i.imgur.com/Q2fA12o.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 21,
+                column: "ImageURL",
+                value: "https://i.imgur.com/R4tMo51.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 22,
+                column: "ImageURL",
+                value: "https://i.imgur.com/a6girSy.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 23,
+                column: "ImageURL",
+                value: "https://i.imgur.com/Qg2sdYM.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 24,
+                column: "ImageURL",
+                value: "https://i.imgur.com/GCqQHi4.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 26,
+                column: "ImageURL",
+                value: "https://i.imgur.com/KcpTFSg.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 27,
+                column: "ImageURL",
+                value: "https://i.imgur.com/TW7rYYS.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 28,
+                column: "ImageURL",
+                value: "https://i.imgur.com/VnCZNRI.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 29,
+                column: "ImageURL",
+                value: "https://i.imgur.com/UbxmtSQ.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 30,
+                column: "ImageURL",
+                value: "https://i.imgur.com/ylFHSxt.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 31,
+                column: "ImageURL",
+                value: "https://i.imgur.com/6sOy4j0.jpg");
+        }
+    }
+}
